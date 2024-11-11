67f901c7f20d326ffdeeb0ecf3f9abd2c763f4a1
Oleg Mkhamadiev <mkhamadiev@mail.ru>
2024-11-11 22:39:45 +0500

    feat: доработать комопонент стрелочной кнопки, начать разрабатывать компонент формы: добавить обработчики, верстать структуру

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -4,24 +4,30 @@ import styles from './App.module.scss';
 
 import { Article } from '../article';
 import { ArticleParamsForm } from '../article-params-form';
-import { defaultArticleState } from '../../constants/articleProps';
+import {
+	ArticleStateType,
+	defaultArticleState,
+} from '../../constants/articleProps';
 
-import { CSSProperties } from 'react';
+import { CSSProperties, useState } from 'react';
 
 export const App = () => {
+	const [appState, setAppState] =
+		useState<ArticleStateType>(defaultArticleState);
+
 	return (
 		<div
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': appState.fontFamilyOption.value,
+					'--font-size': appState.fontSizeOption.value,
+					'--font-color': appState.fontColor.value,
+					'--container-width': appState.contentWidth.value,
+					'--bg-color': appState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm setAppState={setAppState} />
 			<Article />
 		</div>
 	);
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,13 +2,69 @@ import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { FormEvent, useCallback, useState } from 'react';
+import {
+	ArticleStateType,
+	defaultArticleState,
+	OptionType,
+	fontFamilyOptions,
+} from 'src/constants/articleProps';
+import { Text } from '../text';
+import clsx from 'clsx';
+import { Select } from '../select';
 
-export const ArticleParamsForm = () => {
+export type IArticleParamsForm = {
+	setAppState: (value: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = (props: IArticleParamsForm) => {
+	const { setAppState } = props;
+
+	const [isOpen, setIsOpen] = useState<boolean>(false);
+	const [form, setForm] = useState<ArticleStateType>(defaultArticleState);
+
+	const fieldChange = (field: string) => {
+		return (value: OptionType) => {
+			setForm((currentStateForm) => ({
+				...currentStateForm,
+				[field]: value,
+			}));
+		};
+	};
+
+	const handleSubmitForm = (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		setAppState(form);
+	};
+
+	const handleFormReset = (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		setForm(defaultArticleState);
+		setAppState(defaultArticleState);
+	};
+
+	const toggleForm = useCallback(() => {
+		setIsOpen((isOpen) => !isOpen);
+	}, []);
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton isOpened={isOpen} onClick={toggleForm} />
+			<aside
+				className={clsx(styles.container, isOpen && styles.container_open)}>
+				<form
+					className={styles.form}
+					onSubmit={handleSubmitForm}
+					onReset={handleFormReset}>
+					<Text uppercase={true} weight={800} size={31} family='open-sans'>
+						ЗАДАЙТЕ ПАРАМЕТРЫ
+					</Text>
+					<Select
+						title='Шрифт'
+						selected={form.fontFamilyOption}
+						options={fontFamilyOptions}
+						onChange={fieldChange('fontFamilyOption')}
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
